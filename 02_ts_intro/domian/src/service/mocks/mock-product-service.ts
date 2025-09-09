ba4fe92eeb0b21f33a8b82d55a455a0dae1741f1
Maxi Soriano <maxi.soriano.70.23@gmail.com>
2025-09-09 04:12:47 -0300

    Se agrego ejemplo de infer IMG en el backend

---
+++ 02_ts_intro/domian/src/service/mocks/mock-product-service.ts
@@ -0,0 +1,56 @@
+import type { Product } from "../../entities/product.js";
+import type { ProductService } from "../product-service.js";
+
+export class MockProductService implements ProductService {
+    products: Product[] = [];
+
+    constructor(products: Product[]) {
+        this.products = products;
+    }
+
+    async findById(id: string): Promise<Product | undefined> {
+        return this.products.find(product => product.id === id);
+    }
+
+    async findAll(): Promise<Product[]> {
+        return this.products;
+    }
+
+    async editOne(data: Product): Promise<Product> {
+        const index = this.products.findIndex(p => p.id === data.id);
+        if (index !== -1) {
+            this.products[index] = data;
+            return data;
+        }
+        throw new Error("Product not found");
+    }
+
+    async deleteById(id: string): Promise<void> {
+        this.products = this.products.filter(p => p.id !== id);
+    }
+
+    async create(data: Product): Promise<Product> {
+        this.products.push(data);
+        return data;
+    }
+
+    async getProductsWhithDescount(): Promise<Product[]> {
+        return this.products.map(product => ({
+            ...product,
+            price: product.price * 0.9
+        }));
+    }
+
+    async findByName(name: string): Promise<Product | undefined> {
+        return this.products.find(product => product.name === name);
+    }
+
+    async applyDiscount(id: number, discount: number): Promise<Product | undefined> {
+        const product = this.products.find(p => Number(p.id) === id);
+        if (product) {
+            product.price = product.price * (1 - discount);
+            return product;
+        }
+        return undefined;
+    }
+}
--- Readme.md
+++ Readme.md
@@ -1,2 +1,2 @@
 01. Clase 01 Tipos de datos 02/09/2025
-02. Clase 02
+02. Clase 02 Tipos genericos 04/09/2025
