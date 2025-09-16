591b386b3cd0dbc7fe2555aa1d39d01b793c858f
Maxi Soriano <maxi.soriano.70.23@gmail.com>
2025-09-16 01:18:00 -0300

    Ejemplo de omit, pick, exclude funciona e intersecciones, interseccion, estructura de datos recursiva genérica

---
+++ 02_ts_intro/domian/src/entities/mock/product-mock.ts
@@ -0,0 +1,14 @@
+import type { Product } from "../product.js";
+import { fa, faker } from "@faker-js/faker";
+
+export function productMock(opts?: Partial<Product>): Product{
+    return{
+        id: faker.string.uuid(),
+        name: faker.commerce.product(),
+        price: faker.number.int({max: 100000, min: 5000}),
+        ...opts
+    }
+}
+
+// GENERAMOS SOLO COMPUTADORAS
+//productMock({ name: "Computer"})
--- 02_ts_intro/domian/src/entities/user.ts
+++ 02_ts_intro/domian/src/entities/user.ts
@@ -1,8 +1,67 @@
 import type { Entity } from "../utils/types/entity.js";
 
+export const UserStatus = {
+    ACTIVE: "ACTIVE",
+    INACTIVE: "INACTIVE",
+} as const;
+
+export type UserStatus = (typeof UserStatus) [keyof typeof UserStatus];
+
 export interface User extends Entity{
     name: string;
     surname: string;
     email: string;
     password: string;
+    status: UserStatus;
 }
+
+// OMITIMOS LA PASSWORD
+export type SecureUser = Omit<User, "password">
+
+// INCLUIMOS LOS CAMPOS QUE QUEREMOS
+// type SecureUser = Pick<User, "name" | "surname" | "email" | "status">
+
+// Exclude funciona e intersecciones
+// type UserFields = "id" | "name" | "surname" | "email" | "password" | "status";
+
+// export type SecureUserExclude = Exclude<UserFields, "password">;
+
+// INTERSECCION
+/*interface Person{
+    name: string,
+    surname: string
+}
+
+interface LoginFields {
+    email: string,
+    password: string
+}
+
+type UserLogin = Person & LoginFields
+const userTest: UserLogin = {
+    
+}*/
+
+// Estructura de datos recursiva genérica
+/*interface Category<T>{
+    name: string;
+    question?: string;
+    value?: T;
+    subCategories?: Category<T>[];
+}
+
+const examenMatematicas : Category<number> = {
+    name: "Examen de matematicas",
+    subCategories: [
+        {
+            name: "suma",
+            question: "Cuanto es 2 + 2?",
+            value: 4
+        },
+        {
+            name: "resta",
+            question: "cuanto es 2 -2?",
+            value: 0
+        }
+    ]
+}*/
